2aa6b68bdf53c9ba2f88a51b5aaa5a2a2be1be26
mats tornberg <mats.tornberg@gmail.com>
2017-09-28 22:50:57 +0200

    helperfunctions

---
+++ common/index.js
@@ -0,0 +1,31 @@
+module.exports = {
+    isEmptyObject: function (obj) {
+        for(var prop in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+            return false;
+            }
+        }
+        return true;
+    },
+    pathJoin: function() {
+        var ret = [];
+        for(var i in arguments) {
+            ret.push(arguments[i].replace(/\/$/, ""));
+        }
+        return ret.join('/');
+    },
+    each: function (o,cb) {
+        var ret = [];
+        for(i in o) {
+            ret.push(cb(o[i],i));
+        }
+        return ret;
+    },
+    findNode: function(topic,nodes,cb) {
+        nodes.forEach(function(n){
+            if (topic.startWith(n.topic+'/')) {
+                cb(n);
+            }
+        });
+    }
+}
