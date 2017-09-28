c8c64fe89b422edf7409925ea1d842b6f2cb01fb
mats tornberg <mats.tornberg@gmail.com>
2017-09-28 19:36:10 +0200

    cleanup

--- telldus/index.js
+++ telldus/index.js
@@ -7,36 +7,27 @@ var alldevices = [];
 
 function getNodes() {
     telldus.getDevices(function(err,devices) {
-             alldevices = [];
-     
-     
-             if ( err ) {
-                 console.log('Error: ' + err);
-             } else {
-                 
-                 for(var i in devices)
-                 {
-                     var v = devices[i];
-                     //console.log(v);
-                     var topic = "telldus/conf"+v.id;
-                    //  if (i==1){
-                    //      client.subscribe(topic);
-                    //      console.log(topic);
-                    //  }
-                 
-                     alldevices.push({ 
-                        tdid:v.id,
-                        name:v.name,
-                        features:v.methods,
-                        topic:topic
-                    });
-                     
-                 }
-                 console.log('found',alldevices.length);
-                 homeninja.sendNodes(alldevices);
-		 console.log('sent');
-             }
-         });
+        alldevices = [];
+
+        if (err) {
+            console.log('Error: ' + err);
+        } else {
+            for(var i in devices) {
+                var v = devices[i];
+            
+                alldevices.push({ 
+                    tdid:v.id,
+                    name:v.name,
+                    features:v.methods,
+                    topic:"telldus/conf"+v.id
+                });
+                
+            }
+            console.log('found',alldevices.length);
+            homeninja.sendNodes(alldevices);
+		    console.log('sent');
+        }
+    });
 }
 
 homeninja.on('connect',function() {
@@ -44,27 +35,27 @@ homeninja.on('connect',function() {
     homeninja.client.subscribe("telldus/+/+");
 });
  
-homeninja.client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(topic,message.toString());
+homeninja.client.on('message', function (topic, msg) {
+    // message is Buffer
+    var message = msg.toString();
+    console.log(topic,message);
     alldevices.forEach(function(node) {
 	
         if (topic.startsWith(node.topic))
         {
             console.log('found node',message);
-	    var on = (message=="on");
+	        var on = (message=="on");
             if (on) {
-	    telldus.turnOn(node.tdid,function(err) {
-                console.log('deviceId is now ON');
-              });
-	   }else{
-	    telldus.turnOff(node.tdid,function(err) {
-                console.log('deviceId is now OFF');
-              });
-	   }
-
+	            telldus.turnOn(node.tdid,function(err) {
+                    console.log('deviceId is now ON');
+                });
+            }
+            else {
+                telldus.turnOff(node.tdid,function(err) {
+                    console.log('deviceId is now OFF');
+                });
+            }
         }
     });
-  //client.end()
-})
+});
 
