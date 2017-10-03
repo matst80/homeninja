e75b59c8c7f4c2e5cd5a9e8e03893fa4a9271844
mats tornberg <mats.tornberg@gmail.com>
2017-10-03 09:12:03 +0200

    Presence

---
+++ xamarin-app/HomeNinjaApp/NodeViews/Presence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using HomeNinjaApp.Helper;
+using Xamarin.Forms;
+
+namespace HomeNinjaApp.NodeViews
+{
+    [ViewForFeature(new []{"presence"})]
+    public partial class Precense : ContentView, INodeView
+    {
+        
+
+        public bool Test { get; set; }
+
+        async void HandleOn_Clicked(object sender, System.EventArgs e)
+        {
+            await Helper.ServerHelper.Instance.SendBoolStateAsync(itemNode,true);
+        }
+
+        async void HandleOff_Clicked(object sender, System.EventArgs e)
+        {
+            await Helper.ServerHelper.Instance.SendBoolStateAsync(itemNode, false);
+        }
+
+        public OnOff()
+        {
+            InitializeComponent();
+
+        }
+
+        private Node itemNode { get; set; }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            itemNode = BindingContext as Node;
+        }
+    }
+}
