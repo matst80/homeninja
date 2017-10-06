96188188cd78c1c70c9c7f53397eae9928a471a4
mats tornberg <mats.tornberg@gmail.com>
2017-10-06 23:40:00 +0200

    grejjer

---
+++ xamarin-app/HomeNinjaApp/NodeViews/Postition.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace HomeNinjaApp.NodeViews
+{
+    public partial class Postition : ContentView
+    {
+        public Postition()
+        {
+            InitializeComponent();
+        }
+    }
+}
